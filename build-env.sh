148a1c21f5df359a8f3ffe4fb12b0e19a1319f71
aidenlew <aidenmarklewington@gmail.com>
2021-11-09 20:55:12 +0800

    majority build complete

--- build-env.sh
+++ build-env.sh
@@ -20,11 +20,19 @@ brew list iTerm2 &> /dev/null || brew install iTerm2
 brew list tmux &> /dev/null || brew install tmux
 
 # Python3. install and set path of python == python3
+PATH = "$PATH:/usr/local/bin/python3
 
 # Jupyterlab
+brew install jupyterlab
 
 # awscli
+brew install awscli
 
 # Update pip, install global libraries (pd, np, torch etc..)
+pip install --upgrade pip
+pip install pandas numpy torch
 
 # Prompt for git auth and change .gitconfig
+
+
+
